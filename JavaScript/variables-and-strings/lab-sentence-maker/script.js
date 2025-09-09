8c1cf528c0b74171001a8393810ff14cab88ec61
OjasRasam24 <ojasrasam24@gmail.com>
2025-09-09 13:06:18 +0530

    Week3 Day2

---
+++ JavaScript/variables-and-strings/lab-sentence-maker/script.js
@@ -0,0 +1,20 @@
+let adjective, noun, verb, place, adjective2, noun2;
+adjective = "Big";
+noun = "Mouse";
+noun2 = "Cats";
+place = "Cave";
+adjective2 = "MODIFIED";
+verb = "angry";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "Small";
+noun = "Cat";
+noun2 = "Dogs";
+place = "Dog House";
+adjective2 = "little";
+verb = "Mewinggg"
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
