69f19eaf7221dd8f2413559d42794312779c0703
OjasRasam24 <ojasrasam24@gmail.com>
2025-09-09 12:47:10 +0530

    Week3 Day2

---
+++ JavaScript/variables-and-strings/lab-javascript-trivia-bot/script.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName;
+let botLocation;
+let favoriteLanguage;
+botName = "BotOJ";
+botLocation = "90ft Road";
+favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was developed by Brendan Eich in just 10 days in 1995 while working at Netscape Communications.";
+
+console.log(codingFact);
+
+codingFact = 'Initially, it was called "Mocha," then briefly renamed "LiveScript" before finally becoming "' + favoriteLanguage + '".';
+console.log(codingFact);
+
+codingFact = "Despite the similar name, " + favoriteLanguage + " and Java are distinct programming languages with different purposes and designs.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
